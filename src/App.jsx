9b9b8263c511694ba508a195844d303a6c803153
linearmusic <amaannn.gupta.08@gmail.com>
2025-10-16 23:12:44 +0530

    Added the toggle bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { House , Notebook , Code ,PenLine , Github, Linkedin, Twitch, Twitter, Sun, MoonStar ,} from 'lucide-react';
 import Header from "./Components/Header";
-import Toggle from "./Components/Toggle";
 import About from "./Components/About";
 import Experience from "./Components/Experience";
 import Education from "./Components/Education";
@@ -29,13 +29,31 @@ const App = () => {
         <Experience />
         <Education/>
         <Skills/>
+        
+        <div className="fixed bottom-3">
+          <div className='flex flex-row items-center justify-center px-4 py-3 rounded-full bg-white dark:bg-gray-800 border-gray-600 border'>
+        <div className='flex flex-row items-center justify-center gap-3'>
+          <House className="h-5 cursor-pointer"/>
+          <Notebook className="h-5 cursor-pointer"/>
+          <Code className="h-5 cursor-pointer"/>
+          <PenLine className="h-5 cursor-pointer"/>
+        </div>
+
+        <h1 className='font-mono text-black dark:text-white mx-2'>|</h1>
+
+        <div className='flex flex-row items-center justify-center gap-3'>
+          <Github className="h-5 cursor-pointer"/>
+          <Linkedin className="h-5 cursor-pointer"/>
+          <Twitter className="h-5 cursor-pointer"/>
+        </div>
+        <h1 className='font-mono text-black dark:text-white mx-2'>|</h1>
         <button
-          onClick={() => setDarkMode(!darkMode)}
-          className="px-4 py-2 mt-6 rounded-lg bg-gray-200 dark:bg-gray-700 text-gray-900 dark:text-gray-100">
-          Toggle Theme
+          onClick={() => setDarkMode(!darkMode)}>
+            <Sun className="h-5 cursor-pointer block dark:hidden"/>
+            <MoonStar className="h-5 cursor-pointer hidden dark:block"/>   
         </button>
-        <div className="fixed bottom-6">
-          {/* <Toggle /> */}
+          
+    </div>
         </div>
       </div>
 </div>
