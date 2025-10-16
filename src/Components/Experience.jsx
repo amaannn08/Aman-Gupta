4e51201a05e586f41d2a958a7a73765964d3e672
linearmusic <amaannn.gupta.08@gmail.com>
2025-10-16 15:26:33 +0530

    Experience Made better

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -1,22 +1,42 @@
 import React from 'react'
 
 const Experience = () => {
-    
+    const data=[
+        {
+            company:"Zafo Technologies",
+            logo:"./images/zafo.jpeg",
+            role:"Full Stack Engineer",
+            duration:"May 2025 - Jun 2025",
+            link:"https://zafo.ai/"
+        },{
+            company:"Bhaasha.in",
+            logo:"./images/bhaasha.jpeg",
+            role:"Frontend Engineer",
+            duration:"Mar 2025 - Apr 2025",
+            link:"https://www.bhaasha.in/"
+        }
+    ]
     return (
         <div className='w-full flex flex-col mt-5'>
-            <h1 className='font-mono font-bold text-3xl mb-2'>Work Experience</h1>
-            <div className='flex flex-row gap-4 items-center'>
-                <img src="./images/zafo.jpeg" alt="Logo :)" className='rounded-full h-10'/>
-                <div className=' w-full flex justify-between '>
-                    <div className='flex flex-col'>
-                        <h1 className='font-mono font-bold text'>Zafo Technologies</h1>
-                        <h2 className='font-mono text-sm'>Full Stack Engineer</h2>
-                    </div>
-                    <div>
-                        <h1 className='font-mono text-sm dark:text-gray-400 pt-1'>May 2025 - June 2025</h1>
-                    </div>
-                </div>
-            </div>
+            <h1 className='font-mono font-bold text-3xl'>Work Experience</h1>
+            {data.map((item)=>{
+                return (
+                    <> 
+                        <div className='flex flex-row gap-4 items-center my-2'>
+                            <a href={item.link}><img src={item.logo} alt="Logo :)" className='rounded-full cursor-pointer h-10'/></a>
+                            <div className=' w-full flex justify-between cursor-pointer'>
+                                <div className='flex flex-col'>
+                                    <h1 className='font-mono font-bold text'>{item.company}</h1>
+                                    <h2 className='font-mono text-sm'>{item.role}</h2>
+                                </div>
+                                <div>
+                                    <h1 className='font-mono text-sm dark:text-gray-400 pt-1'>{item.duration}</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </>
+                )
+            })}
         </div>
     )
 }
