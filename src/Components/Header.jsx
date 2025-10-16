235a8509acd0419dea04aff49e9e3b7dddffccae
linearmusic <amaannn.gupta.08@gmail.com>
2025-10-16 21:49:24 +0530

    Made it Responsive-1

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,12 +4,12 @@ const Header = () => {
   return (
     <div className='mt-10 w-full flex items-center mb-8 justify-between'>
         <div className="flex flex-col items-start">
-          <h1 className="font-mono text-xl sm:text-4xl md:text-5xl font-bold text-gray-900 dark:text-white">
+          <h1 className="font-mono text-2xl sm:text-4xl md:text-5xl font-bold text-gray-900 dark:text-white">
             Hey, I'm Aman
           </h1>
 
             <ReactTyped
-              className="font-mono text-sm  md:text-2xl text-gray-700 dark:text-gray-300 mt-2"
+              className="font-mono text-md  md:text-2xl text-gray-700 dark:text-gray-300 mt-2"
               strings={[
                 "Software Engineer",
                 "CP Enthusiast",
@@ -20,8 +20,8 @@ const Header = () => {
               backSpeed={40}
               loop
             />
-            <hr className="border-t border-gray-900 dark:border-white mt-3 mb-3 w-full md:w-full" />
-            <h1 className="font-mono text-xs sm:text-sm md:text-xl text-gray-600 dark:text-gray-300 mx-0" >
+            <hr className="border-t border-gray-900 dark:border-white mt-2 md:mt-3 mb-3 w-full md:w-full" />
+            <h1 className="font-mono text-sm sm:text-sm md:text-xl text-gray-600 dark:text-gray-300 mx-0" >
               BS-MS - IIT Kharagpur
             </h1>
         </div>
