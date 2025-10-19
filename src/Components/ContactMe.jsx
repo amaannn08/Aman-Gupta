ecd0ec662e5e58f74c655b7f39e82e64989d561f
linearmusic <amaannn.gupta.08@gmail.com>
2025-10-19 21:11:59 +0530

    Cleaned the code

--- src/Components/ContactMe.jsx
+++ src/Components/ContactMe.jsx
@@ -5,7 +5,7 @@ const ContactMe = () => {
     <div className='mt-10 mb-10 flex flex-col items-center'>
       <h1 className='bg-gray-700 text-white dark:bg-white dark:text-black px-2 rounded-md font-sans font-bold'>Contact</h1>
       <h1 className='font-sans text-2xl font-bold text-center m-2'>Let's Get in Touch!</h1>
-      <h1 className='text-center font-mono text-gray-400'>Want to Chat? just drop me a mail with <a className='text-blue-500' href="amaannn.gupta.08@gmail.com">a direct question</a> and I will respond whenever I can. I will ignore all soliciting!</h1>
+      <h1 className='text-center font-mono text-gray-400'>Want to Chat? just drop me a mail with <br /><a className='text-blue-500' href="mailto:amaannn.gupta.08@gmail.com">a direct question</a><br /> and I will respond whenever I can. <br /> I will ignore all soliciting!</h1>
     </div>
   )
 }
---
+++ src/Components/Maincontent.jsx
@@ -0,0 +1,117 @@
+import React, { useState, useEffect ,useRef } from "react";
+import { House ,Briefcase,CircleUserRound, Notebook , Code , Github, Linkedin, Twitter, Sun, MoonStar, Contact, Loader ,} from 'lucide-react';
+import Header from "./Header";
+import About from "./About";
+import Experience from "./Experience";
+import Education from "./Education";
+import Skills from "./Skills";
+import Projects from "./Projects";
+import ContactMe from "./ContactMe";
+import { motion,useScroll } from "motion/react"
+
+const Maincontent = () => {
+  const scrollProgress=useScroll().scrollYProgress;
+  console.log(scrollProgress);
+  const [selected,setSelected]=useState("");
+  const [profile,setProfile]=useState("");
+  const [darkMode, setDarkMode] = useState(
+    localStorage.getItem("theme") === "dark"
+  );
+  const homeRef = useRef(null);
+  const aboutRef = useRef(null);
+  const projectsRef = useRef(null);
+  const experienceRef = useRef(null);
+  const icons = [
+    { id: "home", Icon: House, ref: homeRef },
+    { id: "briefcase", Icon: Briefcase, ref: experienceRef },
+    { id: "code", Icon: Code, ref: projectsRef },
+    { id: "circleUser", Icon: Contact, ref: aboutRef },
+  ];
+  const profiles=[
+    {id:"github",Icon:Github,link:"https://github.com/amaannn08"},
+    {id:"linkedin",Icon:Linkedin,link:"https://www.linkedin.com/in/aman-gupta-134ab4230/"},
+    {id:"twitter",Icon:Twitter,link:"https://x.com/Amaannn08"},
+  ];
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [darkMode]);
+  
+  const handleScroll = (id, ref) => {
+    setSelected(id);
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  };
+  return (
+    <div>
+          <motion.div 
+          style={{
+            scaleX:scrollProgress
+          }}
+          className="bg-white origin-left rounded-full w-full h-1 top-0 left-0 fixed"></motion.div>
+          <div className=" w-full  transition-all duration-300 flex justify-center">
+          <div className="w-full min-h-screen px-[5%] bg-white text-black dark:bg-gray-900 dark:text-white md:px-[20%] lg:px-[25%] flex flex-col items-center">
+            
+            <div ref={homeRef} className="w-full"><Header /></div>
+            <div  className="w-full"><About /></div>
+            <div ref={experienceRef} className="w-full"><Experience /></div>
+            <div className="w-full"><Education /></div>
+            <div className="w-full"><Skills /></div>
+            <div ref={projectsRef} className="w-full"><Projects /></div>
+            <div ref={aboutRef} className="mt-4 mb-20"><ContactMe/></div>
+
+            <div className="fixed bottom-3">
+              <div className='flex flex-row items-center justify-center px-3 py-3 mx-3 rounded-full bg-white dark:bg-gray-800 border-gray-600 border'>
+                <div className='flex flex-row items-center justify-center '>
+                  {icons.map(({ id, Icon ,ref }) => (
+                      <button key={id} 
+                    className={`px-2 py-2 cursor-pointer transition-all ${
+                          selected === id
+                            ? "bg-gray-300 dark:bg-gray-700  rounded-full"
+                            : "hover:bg-gray-200 dark:hover:bg-gray-800 rounded-full"
+                        }`} onClick={() => handleScroll(id, ref)}>
+                      <Icon
+                        className={`h-5 `}
+                      />
+                    </button>
+                  ))}
+                </div>
+
+                <h1 className='font-mono text-black dark:text-white mx-2'>|</h1>
+
+                <div className='flex flex-row items-center justify-center '>
+                  {profiles.map(({id,Icon,link})=>(
+                    <a href={link}>
+                      <button key={id} onClick={()=>{
+                        setProfile(id)
+                      }}
+                      className={`px-2 py-2 cursor-pointer transition-all ${
+                          profile === id
+                            ? "bg-gray-300 dark:bg-gray-700  rounded-full"
+                            : "hover:bg-gray-200 dark:hover:bg-gray-800 rounded-full"
+                        }`}>
+                          <Icon className="h-5"/>
+                      </button>
+                    </a>
+                  ))}
+                </div>
+                <h1 className='font-mono text-black dark:text-white mx-2'>|</h1>
+                <button
+                  onClick={() => setDarkMode(!darkMode)}>
+                    <Sun className="h-5 cursor-pointer block dark:hidden"/>
+                    <MoonStar className="h-5 cursor-pointer hidden dark:block"/>   
+                </button>
+                
+              </div>
+            </div>
+          </div>
+      </div>
+        </div>
+  )
+}
+
+export default Maincontent
